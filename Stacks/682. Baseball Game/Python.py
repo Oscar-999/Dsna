544e02b3ac60c05f62a160067d44e45e3426b5cb
oscar <oscarvac74@gmail.com>
2023-10-02 19:36:16 -0500

    next day

--- Stacks/682. Baseball Game/Python.py
+++ Stacks/682. Baseball Game/Python.py
@@ -13,3 +13,11 @@ class Solution:
                 stack.append(int(op))
 
         return sum(stack)
+
+
+When should we use stack?
+1. When we want to ensure a system does not move onto another action before completing those before it.
+2. When we want to do something in reverse order.
+3. When we want to implement an undo/redo feature.
+4. When we want to backtrack in searching algos (e.g path finding in a maze)
+5. When we use recursion; it utilizes the call stack
