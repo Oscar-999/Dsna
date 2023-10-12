e49f25c0193ae938badc6db79a01eb9caf892791
Oscar-999 <oscarvac74@gmail.com>
2023-10-12 14:30:03 -0500

    s

---
+++ Arrays/Hashset/217. Contains Duplicate/Python.py
@@ -0,0 +1,32 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            else:
+                hashset.add(n)
+        return False
+
+# on on
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+# n^2 o(1)
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        n = len(nums)
+        for i in range(1, n):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+# nlogn 0(1)
