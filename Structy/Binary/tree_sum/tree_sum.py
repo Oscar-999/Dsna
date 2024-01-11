60dfb5e61ef6e536584704ae69b5210f858e485f
oscar <oscarvac74@gmail.com>
2024-01-10 22:34:24 -0600

    treesum

--- Structy/Binary/tree_sum/tree_sum.py
+++ Structy/Binary/tree_sum/tree_sum.py
@@ -10,3 +10,18 @@ from collections import deque
 
 
 def tree_sum(root):
+    if root is None:
+        return 0
+
+    queue = deque([root])
+    sum = 0
+
+    while queue:
+        node = queue.popleft()
+        sum += node.val
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return sum
