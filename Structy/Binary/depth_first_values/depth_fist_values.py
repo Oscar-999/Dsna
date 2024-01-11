79f3296309402246cbf26757641f1c156ad35327
oscar <oscarvac74@gmail.com>
2024-01-10 21:04:58 -0600

    s

--- Structy/Binary/depth_first_values/depth_fist_values.py
+++ Structy/Binary/depth_first_values/depth_fist_values.py
@@ -22,3 +22,13 @@ def depth_first_values(root):
         if node.left:
             stack.append(node.left)
     return values
+# n = number of nodes
+# Time: O(n^2)
+# Space: O(n)
+def depth_first_values(root):
+    if not root:
+        return []
+    
+    left_values = depth_first_values(root.left)
+    right_values = depth_first_values(root.right)
+    return [ root.val, *left_values, *right_values ]
