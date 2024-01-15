d6b927867b7bb74ccd66afbd5056f210430b037f
oscar <oscarvac74@gmail.com>
2024-01-15 17:51:38 -0600

    s

---
+++ Structy/Binary/all_tree_paths/all_tree_paths.py
@@ -0,0 +1,17 @@
+def all_tree_paths(root):
+  if root is None:
+    return []
+
+  if root.left is None and root.right is None:
+    return [[root.val]]
+
+  paths = []
+
+  left_sub_paths = all_tree_paths(root.left)
+  for sub_paths in left_sub_paths:
+    paths.append([root.val, * sub_paths])
+
+  right_sub_paths = all_tree_paths(root.right)
+  for sub_paths in right_sub_paths:
+    paths.append([root.val, *sub_paths])
+  return paths
