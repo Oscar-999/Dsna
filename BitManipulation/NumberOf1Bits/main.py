0327718eba6a62fcf2f3ae36cbc2b3e1eeb6dee6
oscar <oscarvac74@gmail.com>
2024-01-31 18:00:43 -0600

    bit manipulation

---
+++ BitManipulation/NumberOf1Bits/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self,n:int) -> int:
+        res = 0
+        while n:
+            if n % 2 == 1:
+                res += 1
+            n = n >> 1
+        return res
+#  res += n % 2
+# n = n >> 1
+#  Time and space both 0(1)
+    
