6318c146a56d0c2049843b3e1c4b5e8e344fe37f
oscar <oscarvac74@gmail.com>
2024-01-31 18:06:47 -0600

    sweaty

--- BitManipulation/NumberOf1Bits/main.py
+++ BitManipulation/NumberOf1Bits/main.py
@@ -9,4 +9,12 @@ class Solution:
 #  res += n % 2
 # n = n >> 1
 #  Time and space both 0(1)
-    
+
+    # Sweaty version
+class Solution:
+    def hammingWeight(self,n:int) -> int:
+        res = 0
+        while n:
+            n = n & (n-1)  #can be written as n &= (n-1)
+            res += 1
+        return res
