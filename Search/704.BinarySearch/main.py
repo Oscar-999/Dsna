f3af314a04d553c3a04cf8443810d06d84758e38
Oscar-999 <oscarvac74@gmail.com>
2024-02-13 16:16:29 -0600

    704

--- Search/704.BinarySearch/main.py
+++ Search/704.BinarySearch/main.py
@@ -1,3 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        L, R = 0, len(nums) -1
+
+        while L <= R:
+            mid = L + ((R - L) // 2)
+
+            if  nums[mid] > target:
+                R = mid - 1
+            elif nums [mid] < target:
+                L = mid + 1
+            else:
+                return mid
+        return - 1
