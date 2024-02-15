bae0259c9eaca2208881087653202c03072346a4
oscar <oscarvac74@gmail.com>
2024-02-15 12:51:23 -0600

    875binary

---
+++ Search/875.KokoEatingBananas/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left,right = 1, max(piles)
+        res = right
+
+        while left <= right:
+            k = left + ((right - left)//2)
+            hours = 0
+
+            for p in piles:
+                hours += math.ceil(p/k)
+
+            if hours <= h:
+                res = min(res, k)
+                right = k -1
+            else:
+                left = k + 1
+        return res
