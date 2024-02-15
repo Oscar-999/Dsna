d023f4f5b7c7c0bfc2d6cb01cfee6e46985002e9
oscar <oscarvac74@gmail.com>
2024-02-14 21:30:10 -0600

    searchBst

---
+++ Sorts/700.SearchInABinarySearchTree/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        elif root.val < val:
+            return self.searchBST(root.right,val)
+        
