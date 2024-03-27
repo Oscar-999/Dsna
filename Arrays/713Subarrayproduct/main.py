db98ecdf025e6931056cc049cd1ed5995544afcb
oscar <oscarvac74@gmail.com>
2024-03-27 16:52:11 -0500

    solved

--- Arrays/713Subarrayproduct/main.py
+++ Arrays/713Subarrayproduct/main.py
@@ -1,3 +1,12 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-    
+    l = 0
+    res = 0
+    product = 1
+    for r in range(len(nums)):
+        product *= nums[r]
+        while l<= r and product >= k:
+            product = product // nums[l]
+            l += 1
+        res += (r - l + 1)
+    return res
