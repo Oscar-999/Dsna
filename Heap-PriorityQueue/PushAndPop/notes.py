20efbf48f624628921d5e688efb83a2bd7a1e96d
oscar <oscarvac74@gmail.com>
2024-04-29 14:46:03 -0500

    heap notes

--- Heap-PriorityQueue/PushAndPop/notes.py
+++ Heap-PriorityQueue/PushAndPop/notes.py
@@ -19,3 +19,61 @@ class Heap:
             return None
         if len(self.heap) == 2:
             return self.heap.pop()
+
+        res = self.heap[1]
+        # Move last value to root
+        self.heap[1] = self.heap.pop()
+        i = 1
+        # Percolate down
+        while 2 * i < len(self.heap):
+            if (2 * i + 1 < len(self.heap) and
+            self.heap[2 * i + 1] < self.heap[2 * i] and
+            self.heap[i] > self.heap[2 * i + 1]):
+                # Swap right child
+                tmp = self.heap[i]
+                self.heap[i] = self.heap[2 * i + 1]
+                self.heap[2 * i + 1] = tmp
+                i = 2 * i + 1
+            elif self.heap[i] > self.heap[2 * i]:
+                # Swap left child
+                tmp = self.heap[i]
+                self.heap[i] = self.heap[2 * i]
+                self.heap[2 * i] = tmp
+                i = 2 * i
+            else:
+                break
+        return res
+
+    def top(self):
+        if len(self.heap) > 1:
+            return self.heap[1]
+        return None
+
+    def heapify(self, arr):
+        # 0-th position is moved to the end
+        arr.append(arr[0])
+
+        self.heap = arr
+        cur = (len(self.heap) - 1) // 2
+        while cur > 0:
+            # Percolate down
+            i = cur
+            while 2 * i < len(self.heap):
+                if (2 * i + 1 < len(self.heap) and
+                self.heap[2 * i + 1] < self.heap[2 * i] and
+                self.heap[i] > self.heap[2 * i + 1]):
+                    # Swap right child
+                    tmp = self.heap[i]
+                    self.heap[i] = self.heap[2 * i + 1]
+                    self.heap[2 * i + 1] = tmp
+                    i = 2 * i + 1
+                elif self.heap[i] > self.heap[2 * i]:
+                    # Swap left child
+                    tmp = self.heap[i]
+                    self.heap[i] = self.heap[2 * i]
+                    self.heap[2 * i] = tmp
+                    i = 2 * i
+                else:
+                    break
+            cur -= 1
+
